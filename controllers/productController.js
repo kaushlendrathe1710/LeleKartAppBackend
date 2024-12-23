b65219517ebe31f2319e35d3d729064cc57375e0
manojmksmanu <manoj2022019@gmail.com>
2024-12-23 20:54:38 +0530

    created route for getproductwithcategory

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,8 @@
 // backend/src/controllers/auth.js
 const { db } = require("../config/db/db");
-const { eq } = require("drizzle-orm");
+const { products } = require("../config/db/schema");
+
+const { eq, sql } = require("drizzle-orm");
 const getBanners = async (req, res) => {
   try {
     const banners = await db.query.banners.findMany();
@@ -12,7 +14,7 @@ const getBanners = async (req, res) => {
 };
 const getCategories = async (req, res) => {
   try {
-    const categories =  await db.query.categories.findMany({
+    const categories = await db.query.categories.findMany({
       with: {
         subCategories: true,
       },
@@ -24,4 +26,133 @@ const getCategories = async (req, res) => {
   }
 };
 
-module.exports = { getBanners, getCategories };
+const getProductsWithCategory = async (req, res) => {
+  try {
+    const order = await db.query.adminDashboardValues.findFirst();
+    if (!order) {
+      throw new Error("Error Fetching Data");
+    }
+    const rotationOrder = order.rotationOrder;
+
+    if (rotationOrder === "priceHighToLow") {
+      const data = await ctx.db.query.categories.findMany({
+        with: {
+          products: {
+            where: eq(products.isApproved, "accepted"),
+            limit: 6,
+            orderBy: [desc(products.price)],
+            with: {
+              variants: {
+                limit: 1,
+                with: {
+                  variantImages: {
+                    limit: 1,
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+      res.status(200).json({ data: data });
+      return data;
+    }
+    if (rotationOrder === "priceLowToHigh") {
+      const data = await db.query.categories.findMany({
+        with: {
+          products: {
+            where: eq(products.isApproved, "accepted"),
+            limit: 6,
+            orderBy: [asc(products.price)],
+            with: {
+              variants: {
+                limit: 1,
+                with: {
+                  variantImages: {
+                    limit: 1,
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+      res.status(200).json({ data: data });
+      return data;
+    }
+
+    if (rotationOrder === "alphabetical") {
+      const data = await db.query.categories.findMany({
+        with: {
+          products: {
+            where: eq(products.isApproved, "accepted"),
+            limit: 6,
+            orderBy: [asc(products.name)],
+            with: {
+              variants: {
+                limit: 1,
+                with: {
+                  variantImages: {
+                    limit: 1,
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+      res.status(200).json({ data: data });
+      return data;
+    }
+
+    if (rotationOrder === "random") {
+      const data = await db.query.categories.findMany({
+        with: {
+          products: {
+            where: eq(products.isApproved, "accepted"),
+            limit: 6,
+            orderBy: [sql`RANDOM()`],
+            with: {
+              variants: {
+                limit: 1,
+                with: {
+                  variantImages: {
+                    limit: 1,
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+      res.status(200).json({ data: data });
+      return data;
+    }
+
+    const data = await db.query.categories.findMany({
+      with: {
+        products: {
+          where: eq(products.isApproved, "accepted"),
+          limit: 6,
+          with: {
+            variants: {
+              limit: 1,
+              with: {
+                variantImages: {
+                  limit: 1,
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+    res.status(200).json({ data: data });
+    return data;
+  } catch (error) {
+    console.error("Error fetching banners:", error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = { getBanners, getCategories, getProductsWithCategory };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,18 @@
-
 const { authMiddleware } = require("../middleware/auth");
 const express = require("express");
 const {
   getBanners,
   getCategories,
+  getProductsWithCategory,
 } = require("../controllers/productController");
 const router = express.Router();
 
+// form home page only
+
 router.get("/getBanners", getBanners);
 router.get("/getCategories", getCategories);
+router.get("/getProductsWithCategory", getProductsWithCategory);
+
+// for home page only end
 
 module.exports = router;
